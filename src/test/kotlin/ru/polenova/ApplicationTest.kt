3e586f133d868e2b67fa02b7a7558344e62e8f56
Marina Polenova <m.v.polenova@gmail.com>
2020-12-11 11:11:20 +0100

    add config

--- src/test/kotlin/ru/polenova/ApplicationTest.kt
+++ src/test/kotlin/ru/polenova/ApplicationTest.kt
@@ -1,4 +1,83 @@
 package ru.polenova
 
+import io.ktor.application.*
+import io.ktor.config.*
+import io.ktor.http.*
+import io.ktor.http.content.*
+import io.ktor.server.testing.*
+import io.ktor.util.*
+import kotlinx.io.streams.asInput
+import org.junit.Test
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 class ApplicationTest {
+    private val jsonContentType = ContentType.Application.Json.withCharset(Charsets.UTF_8)
+    private val multipartBoundary = "***blob***"
+    private val multipartContentType =
+        ContentType.MultiPart.FormData.withParameter("boundary", multipartBoundary).toString()
+    private val uploadPath = "./uploads"
+
+    @KtorExperimentalAPI
+    private val configure: Application.() -> Unit = {
+        (environment.config as MapApplicationConfig).apply {
+            put("polenova.upload.dir", uploadPath)
+            put("polenova.fcm.password", "TEST_FCM_PASSWORD")
+            put("polenova.fcm.salt", "TEST_FCM_SALT")
+            put("polenova.fcm.db-url", "TEST_FCM_DB_URL")
+            put("polenova.fcm.path", "./fcm/fcm-encrypted.json")
+        }
+        module(testing = true)
+    }
+    /*@KtorExperimentalAPI
+    @Test
+    fun testAuth() {
+        withTestApplication(configure) {
+            with(auth()) {
+                kotlin.test.assertEquals(io.ktor.http.HttpStatusCode.OK, response.status())
+            }
+        }
+    }
+
+    @KtorExperimentalAPI
+    @Test
+    fun testUpload() {
+        withTestApplication(configure) {
+            val token = with(auth()) {
+                com.jayway.jsonpath.JsonPath.read<String>(response.content!!, "$.token")
+            }
+            with(handleRequest(HttpMethod.Post, "/api/v1/media") {
+                addHeader(HttpHeaders.ContentType, multipartContentType)
+                addHeader(HttpHeaders.Authorization, "Bearer $token")
+                setBody(
+                    multipartBoundary,
+                    listOf(
+                        PartData.FileItem({
+                            Files.newInputStream(Paths.get("./src/test/resources/test.png"))
+                                .asInput()
+                        }, {}, headersOf(
+                            HttpHeaders.ContentDisposition to listOf(
+                                ContentDisposition.File.withParameter(
+                                    ContentDisposition.Parameters.Name,
+                                    "file"
+                                ).toString(),
+                                ContentDisposition.File.withParameter(
+                                    ContentDisposition.Parameters.FileName,
+                                    "photo.png"
+                                ).toString()
+                            ),
+                            HttpHeaders.ContentType to listOf(ContentType.Image.PNG.toString())
+                        )
+                        )
+                    )
+                )
+            }) {
+                response
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertTrue(response.content!!.contains("\"id\""))
+            }
+        }
+    }*/
 }
